1aa3127018f08b30ef48fae240b0cb2218595318
vsoch <vsoch@users.noreply.github.com>
2023-09-13 22:58:54 -0600

    clean up logic to use variable for ldflags
    I figured out that I could write a file, and
    instead simply execute that instead of trying
    to deal with escaped spaces in add_custom_command.
    This also cleans up some of the extra/unsused logic.
    
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- cmake/GolangSimple.cmake
+++ cmake/GolangSimple.cmake
@@ -1,38 +1,23 @@
-
 set(CUSTOM_GO_PATH "${CMAKE_SOURCE_DIR}/resource/reapi/bindings/go")
 set(GOPATH "${CMAKE_CURRENT_BINARY_DIR}/go")
-
-# This probably isn't necessary, although if we could build fluxcli into it maybe
+set(G0111MODULE "off")
+set(GOOS "linux")
 file(MAKE_DIRECTORY ${GOPATH})
 
-# GO_GET will retrieve a go module that needs to be installed
-function(GO_GET TARG)
-  add_custom_target(${TARG} env GOPATH=${GOPATH} go get ${ARGN})
-endfunction(GO_GET)
-
-# ADD_GO_INSTALLABLE_PROGRAM builds a custom go program (primarily for testing)
-function(ADD_GO_INSTALLABLE_PROGRAM NAME MAIN_SRC CGO_CFLAGS CGO_LIBRARY_FLAGS)
+# BUILD_GO_PROGRAM builds a custom go program (primarily for testing)
+function(BUILD_GO_PROGRAM NAME MAIN_SRC GO_BUILD_SCRIPT CGO_CFLAGS CGO_LIBRARY_FLAGS)
   message(STATUS "GOPATH: ${GOPATH}")
-  message(STATUS "CGO_LDFLAGS (before): ${CGO_LIBRARY_FLAGS}")
-  message(STATUS "TEST_FLAGS: ${TEST_FLAGS}")
+  message(STATUS "CGO_LDFLAGS: ${CGO_LIBRARY_FLAGS}")
   get_filename_component(MAIN_SRC_ABS ${MAIN_SRC} ABSOLUTE)
+  get_filename_component(GO_BUILD_SCRIPT_ABS ${GO_BUILD_SCRIPT} ABSOLUTE)
   add_custom_target(${NAME})
 
-  # string(REPLACE <match-string> <replace-string> <out-var> <input>...)
-  STRING(REPLACE ";" " " CGO_LDFLAGS "${CGO_LIBRARY_FLAGS}")  
-
-  # set(ENV{<variable>} [<value>]) as environment OR without CMake variable
-  # Note that I couldn't get this to work (the spaces are always escaped) so I hard coded for now
-  # We need a solution that takes the CGO_LIBRARY_FLAGS arg, and can pass (with spaces not escaped) to add_custom_command
-  SET ($ENV{CGO_LDFLAGS} "${CGO_LDFLAGS}")
-  message(STATUS "CGO_LDFLAGS (after): ${CGO_LDFLAGS}")
-  # SET (CMAKE_GO_FLAGS "${CGO_LDFLAGS}")
-
+  # Find bash to run custom command
+  find_program (BASH_EXECUTABLE bash)
+  file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/${GO_BUILD_SCRIPT}" "GOPATH=${GOPATH}:${CUSTOM_GO_PATH} GOOS=${GOOS} G0111MODULE=${G0111MODULE} CGO_CFLAGS=${CGO_CFLAGS} CGO_LDFLAGS='${CGO_LIBRARY_FLAGS}' go build -ldflags -w -o ${CMAKE_CURRENT_SOURCE_DIR}/${NAME}")
   add_custom_command(TARGET ${NAME}
-                    COMMAND GOPATH=${GOPATH}:${CUSTOM_GO_PATH} GOOS=linux G0111MODULE=off CGO_CFLAGS="${CGO_CFLAGS}" CGO_LDFLAGS='-L${CMAKE_BINARY_DIR}/resource/reapi/bindings -L${CMAKE_BINARY_DIR}/resource/libjobspec/ -ljobspec_conv -lreapi_cli -L${CMAKE_BINARY_DIR}/resource -lresource -lflux-idset -lstdc++ -lczmq -ljansson -lhwloc -lboost_system -lflux-hostlist -lboost_graph -lyaml-cpp' go build -ldflags '-w'
-                    -o "${CMAKE_CURRENT_SOURCE_DIR}/${NAME}"
-                    ${CMAKE_GO_FLAGS} ${MAIN_SRC}
-                    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+                    COMMAND ${BASH_EXECUTABLE} ${GO_BUILD_SCRIPT_ABS}
+                    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
                     DEPENDS ${MAIN_SRC_ABS}
                     COMMENT "Building Go library")
   foreach(DEP ${ARGN})
@@ -41,4 +26,4 @@ function(ADD_GO_INSTALLABLE_PROGRAM NAME MAIN_SRC CGO_CFLAGS CGO_LIBRARY_FLAGS)
   
   add_custom_target(${NAME}_all ALL DEPENDS ${NAME})
   install(PROGRAMS ${CMAKE_CURRENT_BINARY_DIR}/${NAME} DESTINATION bin)
-endfunction(ADD_GO_INSTALLABLE_PROGRAM)
+endfunction(BUILD_GO_PROGRAM)
