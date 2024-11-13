2159feaa10547747a3356346ead3ca4aa06f8dc8
vsoch <vsoch@users.noreply.github.com>
2024-11-12 18:42:29 -0700

    test dan code
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- resource/reapi/bindings/c/reapi_cli.cpp
+++ resource/reapi/bindings/c/reapi_cli.cpp
@@ -289,14 +289,17 @@ extern "C" const char *reapi_cli_get_err_msg (reapi_cli_ctx_t *ctx)
 {
     std::string err_buf = "";
 
-    if (ctx->rqt)
-        err_buf = ctx->rqt->get_resource_query_err_msg () + reapi_cli_t::get_err_message ()
-                  + ctx->err_msg;
-    else
-        err_buf = reapi_cli_t::get_err_message () + ctx->err_msg;
+    if (!ctx || !ctx->rqt) {
+        errno = EINVAL;
+        return "ERROR: REAPI context and/or rqt null \n";
+    }
+
+    err_buf = ctx->rqt->get_resource_query_err_msg () + reapi_cli_t::get_err_message ()
+                + ctx->err_msg;
 
     return strdup (err_buf.c_str ());
 }
+
 
 extern "C" void reapi_cli_clear_err_msg (reapi_cli_ctx_t *ctx)
 {
